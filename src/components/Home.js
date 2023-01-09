1ddd0fff674b7ed81fa205c6d55163d6321f352a
KKTA22 <kushal.kant@tigeranalytics.com>
2023-01-10 01:42:40 +0530

    formatted code and added readme

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import Todo from "./todo"
-import { auth } from '../services/firebase'
+import React from "react";
+import Todo from "./todo";
+import { auth } from "../services/firebase";
 
-import '../App.css';
+import "../App.css";
 
 const Home = ({ user }) => {
   return (
     <div className="home">
-      <h1>Hello, <span></span>{user.displayName}</h1>
+      <h1>
+        Hello, <span></span>
+        {user.displayName}
+      </h1>
       <img src={user.photoURL} alt="" />
-      <button className="button signout" onClick={() => auth.signOut()}>Sign out</button>
-      <Todo/>
+      <button className="button signout" onClick={() => auth.signOut()}>
+        Sign out
+      </button>
+      <Todo />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
--- src/components/todo.js
+++ src/components/todo.js
@@ -1,67 +1,68 @@
-import { useState, useEffect } from 'react'
-import '../App.css';
+import { useState, useEffect } from "react";
+import "../App.css";
 
+const Todo = () => {
+  const [tasks, settasks] = useState([
+    { nam: "sign in with google", comp: false },
+    { nam: "enjoy", comp: false },
+  ]);
+  const [ntask, setntask] = useState("");
 
-const Todo=()=>{
-
-    const [tasks,settasks]= useState([{nam:"sign in with google",comp:false},{nam:"enjoy",comp:false}])
-    const [ntask,setntask]= useState("")
-
-    const chck=()=>{
-        let nams = tasks.map(item=>item.nam)
-        if (nams.includes(ntask)){
-            return true
-        }
-        return false
-
+  const chck = () => {
+    let nams = tasks.map((item) => item.nam);
+    if (nams.includes(ntask)) {
+      return true;
     }
+    return false;
+  };
 
-    const subhand=(event)=>{
-        event.preventDefault()
+  const subhand = (event) => {
+    event.preventDefault();
 
-        if (ntask===""){
-            alert("empty text")
-        }
-        else if (chck()) {
-            console.log("entered2")
-            alert("duplicates not allowed")
-        }
-        else {
-
-            settasks([{nam:ntask,comp:false},...tasks])
-            setntask("")
-        }
+    if (ntask === "" || ntask.replaceAll(' ', '').length === 0) {
+      alert("empty text");
+    } else if (chck()) {
+      console.log("entered2");
+      alert("duplicates not allowed");
+    } else {
+      settasks([{ nam: ntask, comp: false }, ...tasks]);
+      setntask("");
     }
+  };
 
+  const subdel = (event) => {
+    event.preventDefault();
+    settasks(
+      tasks.map((item) =>
+        item.nam === event.target.attributes.t.value
+          ? { ...item, comp: !item.comp }
+          : item
+      )
+    );
+  };
 
-
-
-    const subdel=(event)=>{
-        event.preventDefault()
-        settasks(tasks.map(item=>item.nam===event.target.attributes.t.value?{...item,comp:!item.comp}:item))
-    }
-
-
-
-    return (
-        <div className="todo">
-
-        <form type="submit" className="inputform" onSubmit={subhand}>
-        <input type="text" value={ntask} onChange={(ef) => setntask(ef.target.value)}></input>
+  return (
+    <div className="todo">
+      <form type="submit" className="inputform" onSubmit={subhand}>
+        <input
+          type="text"
+          value={ntask}
+          onChange={(ef) => setntask(ef.target.value)}
+        ></input>
         <input type="submit"></input>
-        </form>
+      </form>
 
-
-        {tasks.map((task)=>(
-            <div className={task.comp?"yes":"no"} key={task.nam}>
-            <h2 className="taskc">{task.nam} </h2>
-            <h3>completed:{task.comp?"yes":"no"}</h3>
-            <button t={task.nam} onClick={subdel}> toggle completetion </button>
-            </div>
-        ))}
+      {tasks.map((task) => (
+        <div className={task.comp ? "yes" : "no"} key={task.nam}>
+          <h2 className="taskc">{task.nam} </h2>
+          <h3>completed:{task.comp ? "yes" : "no"}</h3>
+          <button t={task.nam} onClick={subdel}>
+            {" "}
+            toggle completetion{" "}
+          </button>
         </div>
-    )
-
-
-}
+      ))}
+    </div>
+  );
+};
 export default Todo;
